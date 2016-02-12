8c2bc7316fbaea0c1b9c0b83dd72d611f2476b4a
Artem Lopata <biozshock@gmail.com>
2016-02-12 17:52:40 +0200

    Added gulp watch to gulpfile.js sample

--- Resources/gulp/gulpfile.js
+++ Resources/gulp/gulpfile.js
@@ -1,25 +1,29 @@
 /*
-required modules to run this gulpfile
-maybe some of them are not actually that required :)
+ required modules to run this gulpfile
+ maybe some of them are not actually that required :)
 
-sudo npm install -g gulp-less gulp-logger gulp gulp-cache gulp-concat gulp-debug \
+ sudo npm install -g gulp-less gulp-logger gulp gulp-cache gulp-concat gulp-debug \
  gulp-filter gulp-load-plugins gulp-sourcemaps gulp-uglify gulp-util gulp-changed \
- gulp-if merge-stream path del gulp-rewrite-css gulp-cssnano
+ gulp-if merge-stream path del gulp-rewrite-css gulp-cssnano gulp-watch gulp-plumber
 
  */
 
 /*
-dev launch, no minify and strip:
-$ gulp
+ dev launch, no minify and strip:
+ $ gulp
 
-prod launch to strip, minify and stuff
-$ gulp --env=prod
+ prod launch to strip, minify and stuff
+ $ gulp --env=prod
 
-to output processed files
-$ gulp --debug=true
+ to output processed files
+ $ gulp --debug=true
+
+ to output sourcemaps
+ $ gulp --sourcemaps=true
  */
-
+"use strict";
 var gulp = require('gulp'),
+    gulpDebug = require('gulp-debug'),
     concat = require('gulp-concat'),
     gutil = require('gulp-util'),
     less = require('gulp-less'),
@@ -27,52 +31,78 @@ var gulp = require('gulp'),
     cssnano = require('gulp-cssnano'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify'),
-    changed = require('gulp-changed'),
     merge = require('merge-stream'),
     rewriteCSS = require('gulp-rewrite-css'),
+
+    plumber = require('gulp-plumber'),
+    watch = require('gulp-watch'),
+
+    gnotify = require('gulp-notify'),
+
     config = require('./gulp_config.json'),
 
     del = require('del'),
 
     env = gutil.env.env,
-    debug = gutil.env.debug;
+    debug = gutil.env.debug,
+    generateSourcemaps = gutil.env.sourcemaps,
 
-var gulpIt = function (resource) {
+    onError = function (err) {
+        gutil.beep();
+        if (debug) {
+            gnotify("Something is Wrong!");
+        }
+        console.log(err.toString());
+        this.emit('end');
+    };
+
+var gulpIt = function (resource, watchTask) {
 
     var type = resource.types[0],
         destination = resource.destination.path,
-        gulpy = gulp.src(resource.sources)
-        // the `changed` task needs to know the destination directory
-        // upfront to be able to figure out which files changed
-        .pipe(changed(destination))
-        ;
+        gulpy = gulp.src(resource.sources),
+        file = resource.destination.file;
+
+    gulpy.pipe(plumber({errorHandler: onError}));
+
+    if (watchTask) {
+        gulpy.pipe(watch(resource.sources, {usePolling: true}, function(file) {
+            return gulpIt(resource, false);
+        }));
+    }
+
+    if (debug) {
+        gulpy.pipe(gulpDebug());
+    }
 
     if (type == 'css' || type == 'less') {
         gulpy
-            .pipe(gulpif(env === 'prod', sourcemaps.init()))
-            .pipe(gulpif(/[.]less/, less()))
+            .pipe(gulpif(generateSourcemaps, sourcemaps.init()))
+            .pipe(gulpif(/[.]less/, less().on('error', onError)))
+            .on('error', onError)
             .pipe(rewriteCSS({destination: destination}))
-            .pipe(concat(resource.destination.file))
+            .pipe(concat(file))
             .pipe(gulpif(env === 'prod', cssnano()))
-            .pipe(gulpif(env == 'prod', sourcemaps.write('.')))
+            .pipe(gulpif(generateSourcemaps, sourcemaps.write('.')))
             .pipe(gulp.dest(destination));
 
     } else if (type === 'js') {
         gulpy
-            .pipe(gulpif(env === 'prod', sourcemaps.init()))
             .pipe(gulpif(env === 'prod', uglify()))
-            .pipe(concat(resource.destination.file))
-            .pipe(gulpif(env === 'prod', sourcemaps.write('.')))
+            .pipe(concat(file))
             .pipe(gulp.dest(destination));
     }
 
     if (debug) {
-        console.log(destination, resource.destination.file);
+        console.log(destination, file);
+        gulpy.pipe(gnotify({ message: "finished.", onLast: true }));
     }
 
-    return gulpy;
+    return gulpy.on('error', onError);
 
 };
+
+
 
 gulp.task('clean', function() {
     if (env !== 'prod') {
@@ -85,9 +115,22 @@ gulp.task('default', ['clean'], function() {
 
     console.log('Dumping assets in ' + (env === 'prod' ? 'production' : 'development') + ' mode');
 
-    var tasks = config.map(gulpIt);
+    var tasks = config.map(function(files) {
+        return gulpIt(files, false);
+    });
 
     return merge(tasks);
 
 });
 
+gulp.task('watch', function(done) {
+
+    console.log('Watching assets in ' + (env === 'prod' ? 'production' : 'development') + ' mode');
+
+    var tasks = config.map(function(resource) {
+        return gulpIt(resource, true);
+    });
+
+    return merge(tasks);
+
+});
